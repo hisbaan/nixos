98cc8705a9a35c1d276df0a79a1ead4f0ca7609c
hisbaan <hisbaan@gmail.com>
2024-05-28 12:22:15 -0400

    Update configuration for lab
    setup:
    - systemd-boot
    - nvidia drivers
    - latex
    - steam
    - ddclient
    - drive mounting
    disable:
    - networkmanager

--- lab/configuration.nix
+++ lab/configuration.nix
@@ -8,8 +8,9 @@ in {
   ];
 
   # Bootloader.
-  boot.loader.grub.enable = true; boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.systemd-boot.consoleMode = "max";
 
   # Polkit
   security.polkit.enable = true;
@@ -19,20 +20,19 @@ in {
   ##############
 
   networking.wireless.iwd.enable = true;
-  networking.networkmanager.wifi.backend = "iwd";
-  networking.networkmanager.enable = true;
   networking.hostName = "lab";
 
   # /etc/hosts
-  networking.extraHosts = ''
-    192.168.2.10 archen
-    192.168.2.21 mini
-  '';
+  networking.hosts = {
+    "192.168.1.11" = [ "archen" ];
+    "192.168.1.12" = [ "mini" ];
+  };
 
   # TODO setup firewall
   networking.firewall.enable = false;
 
   services.openssh.enable = true;
+  programs.wavemon.enable = true;
 
   #############
   # Bluetooth #
@@ -65,6 +65,19 @@ in {
     pulse.enable = true;
   };
 
+  ##########
+  # Nvidia #
+  ##########
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+  };
+
   ########
   # Xorg #
   ########
@@ -73,19 +86,27 @@ in {
   services.xserver.autorun = true;
   services.xserver.displayManager.startx.enable = true;
   services.xserver.windowManager.bspwm.enable = true;
-  services.xserver = { layout = "us"; xkbVariant = ""; };
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+    videoDrivers = [ "nvidia" ];
+  };
 
   ############
   # Hyprland #
   ############
 
   programs.hyprland = {
+    package = unstable.hyprland;
     enable = true;
     xwayland.enable = true;
   };
 
-  hardware.opengl.enable = true;
-  hardware.nvidia.modesetting.enable = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
 
   xdg.portal = {
     enable = true;
@@ -132,7 +153,6 @@ in {
     description = "Hisbaan Noorani";
     extraGroups = [
       "networkmanager"
-      "docker"
       "wheel"
       "power"
     ];
@@ -161,6 +181,15 @@ in {
       dataDir = "/home/hisbaan/Documents";
       configDir = "/home/hisbaan/.config/syncthing";
     };
+    ddclient = {
+      enable = true;
+      use = "web, web='https://cloudflare.com/cdn-cgi/trace', web-skip='ip='";
+      protocol = "cloudflare";
+      zone = "hisbaan.com";
+      username = "token";
+      passwordFile = "/home/hisbaan/services/secrets/ddclient";
+      domains = [ "ddns.hisbaan.com" ];
+    };
     printing = {
       enable = true;
     };
@@ -171,7 +200,15 @@ in {
     };
   };
 
+  services.gvfs.enable = true;
+
+  services.fstrim.enable = true;
+
   programs.kdeconnect.enable = true;
+
+  # docker
+  virtualisation.docker.enable = true;
+  users.extraGroups.docker.members = [ "hisbaan" ];
 
   ############
   # Packages #
@@ -185,11 +222,26 @@ in {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # programs.steam = {
-  #   enable = true;
-  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  # };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    package = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
+      ];
+    };
+  };
+  programs.gamescope.enable = true;
 
   fonts.packages = with pkgs; [
     meslo-lg
@@ -225,7 +277,6 @@ in {
     binutils
     cmake
     direnv
-    docker
     gcc
     gdb
     git
@@ -239,9 +290,9 @@ in {
 
     # tex
     biber
-    # texlive.combine {
-    #   inherit (pkgs.texlive) scheme-minimal latex-bin latexmk fontspec;
-    # }
+    (pkgs.texlive.combine {
+      inherit (pkgs.texlive) scheme-minimal latex-bin latexmk fontspec;
+    })
 
     # apps
     discord
@@ -255,25 +306,28 @@ in {
     spotify
     webcord
     zathura
+    cinnamon.nemo
 
     # terminals
-    alacritty
+    unstable.alacritty
     kitty
     wezterm
 
     # system
     efibootmgr
     polkit_gnome
+    shared-mime-info
 
     # wayland
     grimblast
+    swappy
     unstable.hypridle
     unstable.hyprlock
     # hyprsome
     libnotify
     swaybg
     tofi
-    waybar
+    unstable.waybar
     wl-clipboard
 
     # xorg
@@ -282,13 +336,27 @@ in {
     rofi
     sxhkd
     xclip
-    bspwm
+    picom
+
+    # icons/themes
+    capitaine-cursors
+    gnome.adwaita-icon-theme
 
     # misc
-    capitaine-cursors
     pavucontrol
     blueberry
+    unstable.wineWowPackages.stable
+    unstable.winetricks
+    unstable.protontricks
   ];
+
+  environment.etc."current-system-packages".text =
+  let
+    packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
+    sortedUnique = builtins.sort builtins.lessThan (pkgs.lib.lists.unique packages);
+    formatted = builtins.concatStringsSep "\n" sortedUnique;
+  in
+    formatted;
 
   # This value determines the NixOS release from which the default settings for stateful
   # data, like file locations and database versions on your system were taken. It‘s
