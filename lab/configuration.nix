f9eec26da48e3a137a8f7614663f07f2237dac04
hisbaan <hisbaan@gmail.com>
2026-06-21 00:00:30 -0400

    chore: clean up commented out wireplumber config

--- lab/configuration.nix
+++ lab/configuration.nix
@@ -125,15 +125,6 @@ in
     enable = true;
   };
 
-  # services.pipewire.wireplumber.extraConfig = {
-  #   "monitor.bluez.properties" = {
-  #     "bluez5.enable-sbc-xq" = true;
-  #     "bluez5.enable-msbc" = true;
-  #     "bluez5.enable-hw-volume" = true;
-  #     "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
-  #   };
-  # };
-
   #########
   # Audio #
   #########
